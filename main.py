e445697b3685a5c2a97df110078cc21c5a3065ca
13579086422 <119161777+Interactor3000@users.noreply.github.com>
2022-12-03 18:47:19 +0000

    Student live(programm can't read the correct code >:(((   )

--- main.py
+++ main.py
@@ -1,1 +1,82 @@
-print('Hellooo World))')
+# print('Hello world')
+from random import *
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.gladness = 10
+        self.progress = 0
+        self.alive = True
+        self.money = 10
+
+    def say_hello(self):
+        print('Hello!')
+    def to_study(self):
+        print('Time to study')
+        if self.progress < 5:
+            self.gladness -= 4
+        self.progress += 6
+        self.gladness -= 4
+    def to_work(self):
+        self.money += 50
+        self.progres += 2
+        self.gladness -= 3
+    def to_sleep(self):
+        print('Time to sleep')
+        self.gladness += 2
+    def to_eat(self):
+        print('Time to eat')
+        self.gladness += 1
+        self.progress -= 1
+        self.money -= 5
+    def to_smoke(self):
+        print('Time to break health')
+        self.gladness += 1
+        self.progress -= 1
+        self.money -= 5
+    def to_chill(self):
+        print('Rest time')
+        self.gladness += 5
+        self.progress -= 2
+        self.money -= 5
+        def is_alive(self):
+            if self.progress < -10:
+                print('You are bad')
+                self.alive = False
+            elif self.gladness <= 0:
+                print('Dead inside')
+                self.alive = False
+            elif self.progress > 100:
+                print('Amazing! You are so smart')
+                self.alive = False
+            elif self.money <= -100:
+                print('You are a bum')
+                self.alive = False
+    def statics(self):
+        print('Gladness:',self.gladness,  'Progress:',self.progress,  'Is alive:',self.alive,  'Money',self.money)
+    def live(self, day):
+        day = "Day " + str(day) + " of " + self.name + " life"
+        print(day)
+        live_cube = randint(1, 7)
+        
+        if live_cube == 1:
+            self.to_study()
+        elif live_cube == 2:
+            self.to_sleep()
+        elif live_cube == 3:
+            self.to_chill()
+        elif live_cube == 4:
+            self.say_hello()
+        elif live_cube == 5:
+            self.to_eat()
+        elif live_cube == 6:
+            self.to_smoke()
+        elif live_cube == 7:
+            self.to_work()
+        self.statics()
+        self.is_alive()
+
+sanya = Student('Sanya')
+for day in range(365):
+	if sanya.alive == False:
+		break
+	sanya.live(day)
